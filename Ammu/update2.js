5cd25925c6a2e6a72d2081ae6b556447c83c8b2c
Agaath <102163297+Rono-Boy@users.noreply.github.com>
2022-06-03 08:17:27 +0530

    Add files via upload

--- Ammu/update2.js
+++ Ammu/update2.js
@@ -4,7 +4,7 @@ const { promisify } = require('util')
 const { join } = require('path')
 
 let confirmation = {}
-let repository = 'KING-AS/Terror_Queen'
+let repository = 'KING-AS/Terror__Queen'
 let branch = 'master'
 
 async function handler(m, { text, args }) {
